6a1bcdd997ad549deafec508bdbb02cdf13932f5
tilhan-tr <57262142+tilhan-tr@users.noreply.github.com>
2022-02-22 12:58:46 +0300

    Update turak.sh

--- turak.sh
+++ turak.sh
@@ -3,6 +3,7 @@ apt install -y cmake && apt install -y git && apt install -y screen && apt insta
 git clone https://github.com/madMAx43v3r/chia-plotter && cd chia-plotter
 git submodule update --init && bash make_release.sh && mv build/chia_plot /usr/bin
 cd && apt-get install -y python3 python3-pip && apt-get install -y screen git && curl https://rclone.org/install.sh | sudo bash
+python3 -m pip install requests
 mkdir /Tempar && mkdir /Mainar && mkdir /ramdisk
 mount -t tmpfs -o rw,size=119G tmpfs /ramdisk
 wget https://github.com/tilhan-tr/Trinkas/raw/main/egn.rar
